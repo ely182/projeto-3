689a00948eeb635742e5b23e1118aef39ff9c312
ely182 <ely.18gabriel@gmail.com>
2022-12-20 10:31:47 -0300

    style

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { GlobalStyle } from './styles/global-styles';
 import Home from './templates/App';
 
 
@@ -8,6 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Home />
+    <GlobalStyle />
   </React.StrictMode>
 );
 
