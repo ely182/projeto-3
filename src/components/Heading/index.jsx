1bf7990d7a1f877e08334dfc051892bd1028e904
ely182 <ely.18gabriel@gmail.com>
2022-12-25 21:01:05 -0300

    ult

--- src/components/Heading/index.jsx
+++ src/components/Heading/index.jsx
@@ -1,7 +1,7 @@
 import P from 'prop-types'
 import * as styled from './styles';
 
-export const Heading = ({children, colorDark = true, as ='h1', size = 'big',uppercase=false}) =>{
+export const Heading = ({children, colorDark = true, as ='h1', size = 'huge',uppercase=false}) =>{
     return <styled.Title colorDark={colorDark} as ={as} size={size} uppercase={uppercase} >
         {children}
         </styled.Title>;
